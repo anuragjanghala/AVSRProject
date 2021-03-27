2a824e608d5f5f8e7d18c1e219d78e0b43b9c6e9
anuragjanghala <ajanghala@gmail.com>
2021-03-27 00:57:06 +0000

    added dataprep for LRW dataset & Model

---
+++ Data_PreProcessing/for_lrw/convert_audio.py
@@ -0,0 +1,28 @@
+"""
+Transforms mp4 audio to npz. Code has strong assumptions on the dataset organization!
+
+"""
+
+import os
+import glob
+import librosa
+import numpy as np
+
+
+basedir = 'Videos'
+basedir_to_save = 'AUDIO/'
+filenames = glob.glob(os.path.join(basedir, 'lrw','*', '*', '*.mp4'))
+for filename in filenames:
+    data = librosa.load(filename, sr=16000)[0][-19456:]
+    path_to_save = os.path.join(basedir_to_save,
+                                filename.split('/')[-4],
+                                filename.split('/')[-3],
+                                filename.split('/')[-2],
+                                filename.split('/')[-1][:-4]+'.npz')
+    if not os.path.exists(os.path.dirname(path_to_save)):
+        try:
+            os.makedirs(os.path.dirname(path_to_save))
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+    np.savez( path_to_save, data=data)
